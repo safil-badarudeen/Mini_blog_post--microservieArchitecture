aed9dd3f3c9342df427db2e36a0c6954a5b548ad
safil badarudeen <safilbadarudeen@gmail.com>
2023-03-13 18:05:57 +0530

    two service running

---
+++ posts/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crypto');
+
+const app = express();
+const PORT= 4000;
+
+app.use(bodyParser.json())
+
+let posts ={};
+
+
+app.get('/posts', (req, res)=>{
+  res.send(posts)
+})
+
+app.post('/posts',(req, res)=>{
+    const id = randomBytes(4).toString('hex');
+    const {title}= req.body
+
+    posts[id] ={
+        id, title
+    };
+
+    res.send(posts)
+})
+app.listen(PORT,()=>{
+    console.log("listening to port 4000")
+})
