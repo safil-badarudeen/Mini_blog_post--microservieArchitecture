d72675f812d4780f9a259858f461fc9e2b4b8247
safil badarudeen <safilbadarudeen@gmail.com>
2023-03-15 16:01:58 +0530

    Added event bus basic version using axios and express

---
+++ event-bus/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+app.post('/events',(req,res)=>{
+    const events = req.body;
+    axios.post('http://localhost:4000/events',events);
+    axios.post('http://localhost:4001/events',events);
+    axios.post('http://localhost:4002/events',events);
+
+    res.send({status: 'OK'});
+})
+
+
+app.listen(4005, ()=>{
+    console.log("listening to port 4005....")
+})
