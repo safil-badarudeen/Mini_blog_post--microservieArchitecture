8308cefae421bb6c2b924c4f8afe34893f866c3d
safil badarudeen <safilbadarudeen@gmail.com>
2023-03-16 12:53:20 +0530

    added comment status in client side and tested

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,10 +2,22 @@ import React from "react";
 import axios from "axios";
 
 const CommentList = ({ comments }) => {
-  
-
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === "rejected") {
+       content = " This comment is banned";
+    }
+
+    if (comment.status === "pending") {
+       content = "This comment waiting for approval";
+    }
+
+    if (comment.status === "approved") {
+       content = comment.content; 
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
